07edfcf238bb52523b38b5fcfea88cd67dc80f52
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-07 17:20:47 -0500

    Add Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils'
 
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toaster'
 
 export const metadata = {
   title: 'Okapi',
@@ -27,6 +28,7 @@ export default function RootLayout({
         <div className='container max-w-7xl mx-auto h-full pt-12'>
           {children}
         </div>
+        <Toaster />
       </body>
     </html>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link"
 import { Icons } from "./Icons"
 import { buttonVariants } from "./ui/Button"
+import { getAuthSession } from "@/lib/auth"
 
-const Navbar = () => {
+const Navbar = async () => {
+
+  const session = await getAuthSession()
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -13,10 +17,16 @@ const Navbar = () => {
             Okapi
           </p>
         </Link>
+
         {/* search bar */}
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+
+        {session?.user ? (
+          <p>You are logged in</p>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   )
