629215adf0b1a7104d84c5eb9f05d3c8de3c3e13
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-07 13:29:55 -0500

    Add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
+
+import '@/styles/globals.css'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
   title: 'Okapi',
   description: 'An open-source Reddit clone built with Next.js and TypeScript.',
 }
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -15,8 +17,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
-      <body className='min-h-screen p-12 bg-slate-50 antialiased'>{children}</body>
+    <html
+      lang='en'
+      className={cn('bg-white text-slate-900 antialiased light', 
+      inter.className
+      )}>
+      <body className='min-h-screen p-12 bg-slate-50 antialiased'>
+        <Navbar />
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
