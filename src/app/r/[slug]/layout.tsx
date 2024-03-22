7c2fdcdd62247336ff2d5839cd3f0920c81e0bfb
Ian Dodds <ianmitchelldodds@gmail.com>
2024-03-22 11:06:41 -0400

    Add BackButton

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -6,6 +6,7 @@ import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { buttonVariants } from "@/components/ui/Button";
+import BackButton from "@/components/BackButton";
 
 const Layout = async ({
   children,
@@ -57,7 +58,7 @@ const Layout = async ({
   return (
     <div className="sm: container max-w-7xl mx-auto h-full pt-12">
       <div>
-        {/* TODO: Button to take us back */}
+        <BackButton />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <div className="flex flex-col col-span-2 space-y-6">
             {children}
